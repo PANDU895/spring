a3e868086d8c90b57122a066c15f2735e84eff43
learning <learning@DIN66009070.corp.capgemini.com>
2018-02-05 17:15:31 +0530

    mine

---
+++ src/main/java/com/sri/pojo/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.sri.pojo;
+
+public class HelloWorld {
+	private String name;
+	private int mobile;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	public void printHello() {
+		System.out.println("spring 3");
+	}
+
+}
---
+++ src/main/java/com/sri/pojo/Pojo.java
@@ -0,0 +1,19 @@
+package com.sri.pojo;
+
+public class Pojo {
+	private String name;
+	private int mobile;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+
+}
